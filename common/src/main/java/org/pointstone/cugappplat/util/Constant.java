9b1515aea0e7fd8c1f3b160e04767ce8d061ece1
HelloChenJinJun <1981367757@qq.com>
2017-06-28 22:51:38 +0800

    Update Constant.java
    修复bug

--- common/src/main/java/org/pointstone/cugappplat/util/Constant.java
+++ common/src/main/java/org/pointstone/cugappplat/util/Constant.java
@@ -8,15 +8,15 @@ package org.pointstone.cugappplat.util;
  */
 
 public class Constant {
-        public static final String X_API_KEY = "s0048ksww8ck8kc8kg4wc8ooskwsgc00k4kg0kkk";
-        public static final String UNION_ID = "otrNdt4WzWYhfp7CJlErU4M6mH24";
+        public static final String X_API_KEY = "";
+        public static final String UNION_ID = "";
 
-        public static final String WX_APP_ID="wx3569fceef4f16f62";
+        public static final String WX_APP_ID="";
 //        通知id
         public static final int NOTIFY_ID = 100;
 //        IM通讯id和key
         public static final String APP_ID = "";
         public static final String APP_KEY="";
         public static final boolean DEBUG = true;
-        public static String WX_SECRET="ccd91a7c8f0445a800deefcf9a3ab191";
+        public static String WX_SECRET="";
 }
